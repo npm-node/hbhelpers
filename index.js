0bc34babf66063d9754396a048409a279a265b63
yanxianping <yanxianping@haodf.com>
2020-11-03 19:10:18 +0800

    feat: init color helper

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
  * @author      : YanXianPing
  * @creatTime   : 2020/11/3 17:50
  */
+const Handlebars = require('handlebars');
+
 const hbHelpers = {
     /**-----------------------------------------------
      *                   简单helper
@@ -26,10 +28,24 @@ const hbHelpers = {
     toUpperCase: (value) => {
         let _value = value + '';
         return _value.toUpperCase();
-    }
+    },
     /**-----------------------------------------------
      *                   块级helper
      *----------------------------------------------*/
+    color: (context, options) => {
+        const colorMap = {
+            success: '#67C23A',
+            warning: '#E6A23C',
+            danger: '#F56C6C',
+            info: '#909399'
+        };
+        let color = colorMap[context];
+        let attrs = Object.keys(options.hash).map(key => {
+            return `${key}="${options.hash[key]}"`;
+        }).join(' ');
+        let result = `<span ${attrs} style="color: ${color}">${options.fn(this)}</span>`;
+        return new Handlebars.SafeString(result);
+    }
 };
 
 module.exports = hbHelpers;
